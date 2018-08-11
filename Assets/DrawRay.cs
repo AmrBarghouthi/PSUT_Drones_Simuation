3fa27be76d7adac2d5d8833e4ecafbac8fd2e219
Laith Mohammad <laythra@gmail.com>
2018-08-11 16:41:18 +0300

    Moving the drone.

--- Assets/DrawRay.cs
+++ Assets/DrawRay.cs
@@ -13,11 +13,19 @@ public class DrawRay : MonoBehaviour {
 	List<double> pReadingAngles;
 	List<double> pDistanceReadings;
 	List<double> agoraFactors;
+	List<Vector3> avoidingLocation;
 
+	/* Avoiding can happen by either sotring the angle of the target position of the vector3, use whatever function. */
+	Vector3 VectorTarget; // TODO: Set the VectorAngle to the target position using the agoraFactors list.
+	float AngleTarget; // TODO: Set the AngleTarget to the target position using the agoraFactors list.
+
+	[SerializeField]
+	public float speed; // Define it from the inspector
 
 	// Use this for initializing the number of rays to shoot out of the drone.
 	private int RaysToShoot = 30;
 	void Start () {
+		speed = 0.5f;
 		pReadingAngles = new List<double>();
 		StartCoroutine(StartAgoraphilic()); // Waiting for 3 seconds just to make sure the object has been detected if there's any.
 	}
@@ -26,6 +34,7 @@ public class DrawRay : MonoBehaviour {
     {
         yield return new WaitForSeconds(3);
 		agoraFactors = GetAgoraFactors();
+		// Avoid(Angle);
     }
 	
 
@@ -45,7 +54,7 @@ public class DrawRay : MonoBehaviour {
 			Debug.DrawRay(transform.position, dir, Color.red);
 
 			if (Physics.Raycast(transform.position, dir, out hit)) {
-				angle = (float)(angle * (180f/Math.PI));
+				angle = (float)(angle * (180f/Math.PI)); // fix angles calculation.
 				pReadingAngles.Add(angle);
 				DetectedGameObject = hit.collider.gameObject; // That was the object that was hit by the ray.
 			}
@@ -60,6 +69,8 @@ public class DrawRay : MonoBehaviour {
 		GetAngle();
 		bool[] flags = new bool[pReadingAngles.Count];
 		List<double> agoraFactors = new List<double>();
+		List<Vector3> avoidingLocation = new List<Vector3>();
+
 		for(int i = 0; i < pReadingAngles.Count; i++) {
 			flags[i] = true;
 		}
@@ -69,12 +80,14 @@ public class DrawRay : MonoBehaviour {
 		for (int i = 0; i < pReadingAngles.Count; i++) {
 			factors[i] = 1 - (i * subNumber);
 			agoraFactors.Add(0);
+			avoidingLocation.Add(new Vector3(0f, 0f, 0f));
 		}
 
 		double angleDiff;
 		for (int i = 0; i < pReadingAngles.Count; i++) {
 			angleDiff = double.MaxValue;
 			int leastForRound = 0;
+			Vector3 avoidingLoc;
 			for (int j = 0; j < pReadingAngles.Count; j++) {
 				double currentDiff = Math.Abs(pReadingAngles[j] - targetAngle);
 				currentDiff = currentDiff > 180 ? 360 - currentDiff : currentDiff;
@@ -86,9 +99,23 @@ public class DrawRay : MonoBehaviour {
 
 			agoraFactors[leastForRound] = factors[i];
 			flags[leastForRound] = false;
+
+			// Setting the target angle or target vector should happen around here.
+			// and either of the two functions below should be called.
 		}
 
 		return agoraFactors;
+	}
+
+	// Make the gameobject move by the specified angle.
+	private void AvoidByAngle(float target) {
+		float angle = Mathf.MoveTowardsAngle(transform.eulerAngles.y, target, speed * Time.deltaTime);
+        transform.eulerAngles = new Vector3(0, angle, 0);
+	}
+
+	// Make the gameobject move by the specified vector3
+	private void AvoidByVector3(Vector3 target) {
+		transform.position = Vector3.MoveTowards(transform.position, VectorTarget, speed * Time.deltaTime);
 	}
 
 
